5961c90bb785e2b02b18f348262ab25d392d40ce
dp86 <dp86@iu.edu>
2025-02-20 20:54:00 -0500

    practicum 2 added

---
+++ src/main/java/edu/iu/p466/prime_service/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package edu.iu.p466.prime_service.controller;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.iu.p466.prime_service.model.Customer;
+import edu.iu.p466.prime_service.service.IAuthenticationService;
+
+@RestController
+public class AuthenticationController {
+    private final IAuthenticationService authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public boolean register(@RequestBody Customer customer) {
+        try {
+            return authenticationService.register(customer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/edu/iu/p466/prime_service/service/AuthenticationService.java
@@ -0,0 +1,32 @@
+package edu.iu.p466.prime_service.service;
+
+import java.io.IOException;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import edu.iu.p466.prime_service.model.Customer;
+import edu.iu.p466.prime_service.repository.IAuthenticationRepository;
+
+public class AuthenticationService implements IAuthenticationService {
+
+    IAuthenticationRepository authenticationRepository;
+
+    public AuthenticationService(IAuthenticationRepository authenticationRepository) {
+        this.authenticationRepository = authenticationRepository;
+    }
+
+    @Override
+    public boolean register(Customer customer) throws IOException {
+        BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+        String passwordEncoded = bc.encode(customer.getPassword());
+        customer.setPassword(passwordEncoded);
+        return authenticationRepository.save(customer);
+    }
+
+    @Override
+    public boolean login(String username, String password) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+}
